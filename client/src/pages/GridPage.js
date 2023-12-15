ea05e474f8db2cc34cdfe4f05cb9418d14f35caf
Ananya Kulkarni <ananyakulkarni1203@gmail.com>
2023-12-15 18:57:43 +0530

    Files edited

--- client/src/pages/GridPage.js
+++ client/src/pages/GridPage.js
@@ -24,10 +24,17 @@ const GridPage = () => {
   };
 
   return (
-    <div>
-      <h1>Welcome to the Grid Page</h1>
-      <Grid items={items} onAddToCart={handleAddToCart} />
-    </div>
+    <Container>
+      <Row>
+        <Col>1 of 2</Col>
+        <Col>2 of 2</Col>
+      </Row>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
   );
 };
 
