25ffe2260e5cb9c730f3621b5fd8c9977b9dc712
Setu Bhatt <bansetu1@gmail.com>
2023-12-22 23:26:14 +0530

    added checkout

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GridPage from './pages/GridPage';
 import CheckoutPage from './pages/CheckoutPage';
 import InvoicePage from './pages/InvoicePage';
+import Success from './pages/Success';
+import Cancel from './pages/Cancel';
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
         <Route exact path="/" element={<GridPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="/invoice" element={<InvoicePage />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/cancel" element={<Cancel/>} />
       </Routes>
     </Router>
   );
--- client/src/pages/CheckoutPage.js
+++ client/src/pages/CheckoutPage.js
@@ -1,25 +1,95 @@
-// pages/CheckoutPage.js
-import React from 'react';
-import Layout from '../components/common/Layout';
-import CheckoutItem from '../components/CheckoutItem';
+import React, { useEffect, useState } from "react";
 
-const CheckoutPage = () => {
-  const cartItems = [
-    { itemName: 'Item 1', quantity: 2, price: 20 },
-    // Add more items as needed
-  ];
+const formatPrice = ({ amount, currency, quantity }) => {
+  const numberFormat = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency,
+    currencyDisplay: "symbol",
+  });
+  const parts = numberFormat.formatToParts(amount);
+  let zeroDecimalCurrency = true;
+  for (let part of parts) {
+    if (part.type === "decimal") {
+      zeroDecimalCurrency = false;
+    }
+  }
+  amount = zeroDecimalCurrency ? amount : amount / 100;
+  const total = (quantity * amount).toFixed(2);
+  return numberFormat.format(total);
+};
+
+const Checkout = () => {
+  const [quantity, setQuantity] = useState(1);
+  const [amount, setAmount] = useState(0);
+  const [currency, setCurrency] = useState("USD");
+
+  useEffect(() => {
+    async function fetchConfig() {
+      // Fetch config from our backend.
+      const { unitAmount, currency } = await fetch("/config").then((r) =>
+        r.json()
+      );
+      setAmount(unitAmount);
+      setCurrency(currency);
+    }
+    fetchConfig();
+  }, []);
 
   return (
-    <Layout>
-      <h2>Checkout Page</h2>
-      <ListGroup>
-        {cartItems.map((item, index) => (
-          <CheckoutItem key={index} {...item} />
-        ))}
-      </ListGroup>
-      {/* Add total and checkout button */}
-    </Layout>
+    <>
+      <div className="sr-root">
+        <div className="sr-main">
+          <section className="container">
+            <div>
+              
+              <h4>Your Items</h4>
+              <div className="pasha-image">
+                <img
+                  alt="Random asset from Picsum"
+                  src="https://picsum.photos/280/320?random=4"
+                  width="140"
+                  height="160"
+                />
+              </div>
+            </div>
+            <form action="/create-checkout-session" method="POST">
+              <div className="quantity-setter">
+                <button
+                  className="increment-btn"
+                  disabled={quantity === 1}
+                  onClick={() => setQuantity(quantity - 1)}
+                  type="button"
+                >
+                  -
+                </button>
+                <input
+                  type="number"
+                  id="quantity-input"
+                  min="1"
+                  max="10"
+                  value={quantity}
+                  name="quantity"
+                />
+                <button
+                  className="increment-btn"
+                  disabled={quantity === 10}
+                  onClick={() => setQuantity(quantity + 1)}
+                  type="button"
+                >
+                  +
+                </button>
+              </div>
+              <p className="sr-legal-text">Number of copies (max 10)</p>
+
+              <button role="link" id="submit" type="submit">
+                Buy {formatPrice({ amount, currency, quantity })}
+              </button>
+            </form>
+          </section>
+        </div>
+      </div>
+    </>
   );
 };
 
-export default CheckoutPage;
+ export default Checkout;
